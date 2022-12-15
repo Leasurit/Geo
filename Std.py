6d1e22c877306c958157003a7ba81c9be47202a4
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-15 12:40:30 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-
-#df = pd.read_excel("OxAndEl2.xlsx", "Tabelle1")
-
-
 #wb = openpyxl.load_workbook(‘workbook.xlsx’)
 #sheet = wb[‘data’]
 
-
+st.title("Hello world!") 
+df = pd.read_excel("OxAndEl2.xlsx")
+st.write(df)
 st.write("Add your own data:")
 uploadedfile = st.file_uploader("Choose your file")
 df2 = pd.read_excel(uploadedfile)
 
+#df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
