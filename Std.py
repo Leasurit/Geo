c6b455a23fb96dba1b5f2d30ecf6783382edca72
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-15 12:32:26 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import streamlit as st
+import openpyxl
 
-df = pd.read_excel("OxAndEl2.xlsx", "Tabelle1")
+#df = pd.read_excel("OxAndEl2.xlsx", "Tabelle1")
+
+
+#wb = openpyxl.load_workbook(‘workbook.xlsx’)
+#sheet = wb[‘data’]
+
+uploadedfile = st.file_uploader("OxAndEl2")
+df = pd.read_excel(uploadedfile)
+
