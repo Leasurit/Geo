d7b04c30b5879b233ea3b9990bea3e421ec8910b
Lea <116798710+Leasurit@users.noreply.github.com>
2023-01-03 18:29:02 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -17,14 +17,17 @@ standard = st.sidebar.multiselect("Select your standard:", options = df_data["St
 oxides = df_data.iloc[:, 3:25]
 oxide = st.sidebar.multiselect("Select your oxide", options=list(oxides)) #, default=list(df_data.columns[3:25]))
 # Element auswählen
-element = st.sidebar.multiselect("Select your element", options=list(df_data.columns[26:104])) #, default=list(df_data.columns[26:104]))
+elements = df_data.columns[26:104]
+element = st.sidebar.multiselect("Select your element", options=list(elements)) #, default=list(df_data.columns[26:104]))
 
 #Gestein auswählen
 #rocktype = st.sidebar.multiselect("Select your rock type:", options = df_metadata["Rock type"].unique(), default = df_data["Rock type"].unique())
 
 df_data_selection = df_data.query("Standard == @standard")
 st.dataframe(df_data_selection)
+#st.dataframe(df_data_selection = df_data.query("elements == @element"))
 st.write(df_data[:, element])
+#st.wirte(df_data[]
 
 #st.write(df_data.iloc[:, 3:25])
 #st.write(df[oxide])
