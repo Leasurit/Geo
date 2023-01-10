d9d006af89e98861a1e3cd243619641046ce616b
Lea <116798710+Leasurit@users.noreply.github.com>
2023-01-10 12:19:17 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -31,7 +31,7 @@ element = st.sidebar.multiselect("Select an element", options=list(elements)) #,
 st.write("Please select an element / elements to see their concentrations for all standards.")  
 fil = df_data['Constituent'] == 'Concentration'
 df_data_conc_only = df_data[fil]
-furtherinfo = LookUp.loc[['Al'], ['Further information']]
+furtherinfo = LookUp.loc[[element], ['Further information']]
 st.write(df_data_conc_only[["Standard"] + element + furtherinfo])
 
 #if options in LookUp further ist ungleich N/A
