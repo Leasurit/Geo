1124ca81b7e99862c8023b26ddbc0c29566f62f7
Lea <116798710+Leasurit@users.noreply.github.com>
2023-01-10 12:21:39 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -26,8 +26,6 @@ standard = st.sidebar.multiselect("Select a standard:", options = df_data["Stand
 elements = df_data.columns[26:104]
 element = st.sidebar.multiselect("Select an element", options=list(elements)) #, default=list(df_data.columns[26:27]))
 
-
-
 st.write("Please select an element / elements to see their concentrations for all standards.")  
 fil = df_data['Constituent'] == 'Concentration'
 df_data_conc_only = df_data[fil]
