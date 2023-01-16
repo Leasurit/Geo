17037ce746704acedfb9b91f25273f766b4e0428
Lea <116798710+Leasurit@users.noreply.github.com>
2023-01-16 10:35:42 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -13,7 +13,7 @@ st.write(LookUp)
 std_names = df_data['Standard'].drop_duplicates()
 
 LookUp.set_index("Element", inplace=True)
-st.write(LookUp.loc[['Al'], ['Further information']])
+#st.write(LookUp.loc[['Al'], ['Further information']])
 
 #st.write(LookUp['Element'])
 #Element(e) auswählen und Infos aus df_data anzeigen. In LookUp schauen: Wenn nicht N/A in LookUp für diese Element(e) die eingetragenen Werte als Spalten aus df_data anzeigen
@@ -21,6 +21,10 @@ st.write(LookUp.loc[['Al'], ['Further information']])
 st.sidebar.header("Select data:")
 # Standard auswählen
 standard = st.sidebar.multiselect("Select a standard:", options = df_data["Standard"].unique()) #, default = df_data["Standard"].unique())
+
+st.write("Data for selected standard:")
+df_data_selection = df_data.query("Standard == @standard")
+st.dataframe(df_data_selection)
 
 # Element auswählen
 elements = df_data.columns[26:104]
@@ -35,25 +39,10 @@ st.write(df_data_conc_only[["Standard"] + element + furtherinfo])
 #if options in LookUp further ist ungleich N/A
 #  st.write(LookUp['Eintrag in LookUp']
 
+#st.write(df_data_conc_only2[["Standard"] + element])
 
-
-
-## Oxid auswählen
-#oxides = df_data.iloc[:, 3:25]
-#oxide = st.sidebar.multiselect("Select an oxide", options=list(oxides)) #, default=list(df_data.columns[3:25]))
 #Gestein auswählen
 #rocktype = st.sidebar.multiselect("Select your rock type:", options = df_metadata["Rock type"].unique(), default = df_data["Rock type"].unique())
-
-st.write("Data for selected standard:")
-df_data_selection = df_data.query("Standard == @standard")
-st.dataframe(df_data_selection)
-
-
-
-st.write("Please select an oxide / oxides to see their concentrations for all standards.")  
-fil2 = df_data['Constituent'] == 'Concentration'
-df_data_conc_only2 = df_data[fil2]
-st.write(df_data_conc_only2[["Standard"] + oxide])
 
 #st.write(df_data.iloc[:, 3:25])
 #st.write(df[oxide])
@@ -62,11 +51,6 @@ st.write(df_data_conc_only2[["Standard"] + oxide])
   #st.write('Add your own data:')
   #uploadedfile = st.file_uploader("Choose your file")
   #df2 = pd.read_excel(uploadedfile)
-
-## Standard auswählen
-  #Standards = std_names (diese Liste, die wir definiert haben)
-#option = st. selectbox ('Choose a standard:', (std_names))
-#st.write('Selected:', option)
 
 #def SelectData (name):
     #fil  = df_data['Standard'] == name
