1c1267feee0317ac603769f1a874b33469e71cdc
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2023-01-24 11:01:01 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -44,7 +44,7 @@ st.write('---')
 st.write(furtherinfo['Further information'].tolist())
 st.write('---')
 for i in furtherinfo['Further information'].tolist():
-  if i is not NaN:
+  if i is NaN:
     res = i.split('_')
     fullEllist = fullEllist + res
   else:
