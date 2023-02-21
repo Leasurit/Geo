8b4eae695c7870ccdef5287ef2052aa30160a49e
Lea <116798710+Leasurit@users.noreply.github.com>
2023-02-21 13:22:51 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -59,7 +59,12 @@ st.write('fullEllist')
 df_data[fullEllist] # zeigt alle zusätzlichen Spalten zum ausgewählten Element
 df_data[element] # zeigt Spalten aller ausgewählten Elemente
 df_data['Standard'] # zeigt Spalte Standard
-df_data[element + fullEllist]
+dfselected = df_data[element + fullEllist]
+
+dfstd = df_data['Standard']
+dfneu = dfstd.join(dfselected)
+dfneu
+
 
 st.write('dfselected')
 
