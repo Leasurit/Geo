e5cb458655206745cd529e478636e1aa6108efe2
Lea <116798710+Leasurit@users.noreply.github.com>
2023-02-25 17:01:18 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -91,7 +91,7 @@ zeilen = df_data[df_data['Constituent'] == 'Concentration'].index # Jede Zeile z
 for i in element:
   auswahl = df_data.columns.get_loc(i)
   d = df_data.iloc[zeilen, auswahl]
-  fig2 = px.scatter(x=std_names, y=d, log_y=True, title = i, ylabel='Concentration in ppm')
+  fig2 = px.scatter(x=std_names, y=d, log_y=True, title = i, y_label='Concentration in ppm')
   st.plotly_chart(fig2)
   
 
