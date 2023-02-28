1cf74df80c76dad7fba6402ae61f671fa307a176
Lea <116798710+Leasurit@users.noreply.github.com>
2023-02-28 10:19:03 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -97,6 +97,20 @@ for i in element:
 
 # Ende Befehl
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
 #fig1, ax = plt.subplots()
 #ax.scatter(x,y)
 
