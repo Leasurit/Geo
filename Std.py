ae7e6bd75d2d99a881d4add1f8fc491221140f6f
Lea <116798710+Leasurit@users.noreply.github.com>
2023-03-07 14:51:46 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -61,7 +61,7 @@ with tab2:
             res = i.split('_')
          else:
             res = 'none'
-      fullEllist = fullEllist + element + res
+   fullEllist = fullEllist + element + res
       
    st.write(fullEllist)
     
