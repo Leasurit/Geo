f5d60a31f0543ce950407226f222bfe44f489133
Lea <116798710+Leasurit@users.noreply.github.com>
2023-03-16 11:47:27 +0100

    Update Std.py

--- Std.py
+++ Std.py
@@ -137,7 +137,7 @@ with tab2:
       
      #lin = st.checkbox('lin scale') 
      #log = st.checkbox('log scale')
-     if options = lin:
+     if options == lin:
       fig2 = px.scatter(x=std_names, y=d,  title = i)
      else:
       fig2 = px.scatter(x=std_names, y=d, log_y=True,  title = i)
