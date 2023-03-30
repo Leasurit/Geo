6076fc5cefb79193138cc4a98f2e8d543464dbdc
Lea <116798710+Leasurit@users.noreply.github.com>
2023-03-30 12:30:42 +0200

    Update Std.py

--- Std.py
+++ Std.py
@@ -201,7 +201,7 @@ with tab4:
    type = st.radio('Choose a standard', ("CI Chondrite", "E-MORB", "N-MORB", "OIB", "PM"))
    df_ci.set_index("Rock", inplace = True)
    st.write(df_ci)
-   df_meta.loc[type, :]
+   df_ci.loc[type, :]
    
       
 with tab5:
